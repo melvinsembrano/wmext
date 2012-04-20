0f08c396de3c6088fb3d89016cfb398d97892acf
Melvin Sembrano <melvinsembrano@gmail.com>
2012-04-20 18:31:46 +1000

    change video player to object

--- js/wmext.js
+++ js/wmext.js
@@ -53,12 +53,33 @@ var wmext = {
         )
       );
     });
-    $(el).html(ul);
+    $(el).html("<strong>View Video</strong>");
+    $(el).append(ul);
   },
 
   showVideo: function(item) {
+    var obj = $("<object></object>");
+    var height = "390px";
+    var width = "640px";
+
+    obj.css("height", height)
+      .css("width", width)
+      .append($("<param name='movie'></param").attr("value", item.content.src))
+      .append($("<param name='allowFullScreen'></param").attr("value", "true"))
+      .append($("<param name='allowScriptAccess'></param").attr("value", "always"))
+      .append(
+        $("<embed></embed>")
+          .attr("type", "application/x-shockwave-flash")
+          .attr("src", item.content.src)
+          .attr("allowfullscreen", "true")
+          .attr("allowScriptAccess", "always")
+          .attr("width", width)
+          .attr("height", height)
+      );
+
     $.fancybox.open([{
-      content: $("<iframe width='560' height='315' frameborder='0' allowfullscreen></iframe>").attr("src",item.content.src),
+//      content: $("<iframe width='560' height='315' frameborder='0' allowfullscreen></iframe>").attr("src",item.content.src),
+      content: obj,
       title: item.title.$t
     }]);
   },
